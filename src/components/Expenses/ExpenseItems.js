e3abdf65886a7f8e7a14a769c076f0b6a7d0c005
suhasKumar <suhassuhas3468@gmail.com>
2023-12-20 10:54:52 +0530

    useState

---
+++ src/components/Expenses/ExpenseItems.js
@@ -0,0 +1,28 @@
+import React ,{useState} from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItems.css";
+
+export default function ExpenseItems(props) {
+  
+  const [amount,setAmount]=useState(props.amount)
+
+
+  // const [title,SetTitle]=useState(props.title)
+
+  console.log("Expense items Executed")
+  const clickHandler =() =>{
+    setAmount('100')
+    console.log(amount)
+    // SetTitle("Updated")
+    // console.log(title)
+  }
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <h2>{props.title}</h2>
+      <div className="expense-item__price">${amount}</div>
+      <button onClick={clickHandler}>changeTitle</button>
+    </div>
+  );
+}
